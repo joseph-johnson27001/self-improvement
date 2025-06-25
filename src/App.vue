38f31ad2263cab1fd3b9a31e88fef74bdcbc9b10
Joe Johnson <joseph.johnson27001@gmail.com>
2025-06-25 10:49:15 +0700

    Sidebar and initial app build

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <div id="app">Hello World</div>
-  <KPIArea />
-  <GraphArea />
+  <div id="app">
+    <SideBar />
+    <div class="content">
+      <KPIArea />
+      <GraphArea />
+    </div>
+  </div>
 </template>
 
 <script>
 import GraphArea from "./components/Layout/GraphArea.vue";
 import KPIArea from "./components/Layout/KPIArea.vue";
+import SideBar from "./components/Layout/SideBar.vue";
+
 export default {
   name: "App",
   components: {
+    SideBar,
     GraphArea,
     KPIArea,
   },
 };
 </script>
+
+<style>
+#app {
+  display: flex;
+  height: 100vh;
+}
+.content {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+}
+</style>
